ebba904ae3435d7fb8ce3ce33267955217a49fe3
computerwizjared <computerwizjared@hotmail.com>
2014-12-27 09:06:43 -0600

    Config Stuff

--- src/main/java/net/jaredbates/BukkitIssueTracker/BukkitIssueTracker.java
+++ src/main/java/net/jaredbates/BukkitIssueTracker/BukkitIssueTracker.java
@@ -1,10 +1,17 @@
 package net.jaredbates.BukkitIssueTracker;
 
+import lombok.Getter;
 import net.jaredbates.BukkitIssueTracker.Listeners.PlayerJoinListener;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.eclipse.egit.github.core.client.GitHubClient;
 
 public class BukkitIssueTracker extends JavaPlugin {
+    @Getter private GitHubClient gitHubClient = new GitHubClient();
+    @Getter private boolean oAuth = false;
+
     public void onEnable() {
+        saveDefaultConfig();
+        oAuth = getConfig().getBoolean("OAuth.Enabled");
         getServer().getPluginManager().registerEvents(new PlayerJoinListener(this), this);
     }
 
