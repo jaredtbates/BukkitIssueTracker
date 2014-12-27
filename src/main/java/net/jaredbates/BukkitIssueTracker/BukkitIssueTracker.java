c8763ff3614250913030a8a294af3116b7830f5b
computerwizjared <computerwizjared@hotmail.com>
2014-12-27 12:24:44 -0600

    Added Authentication Methods

--- src/main/java/net/jaredbates/BukkitIssueTracker/BukkitIssueTracker.java
+++ src/main/java/net/jaredbates/BukkitIssueTracker/BukkitIssueTracker.java
@@ -7,11 +7,24 @@ import org.eclipse.egit.github.core.client.GitHubClient;
 
 public class BukkitIssueTracker extends JavaPlugin {
     @Getter private GitHubClient gitHubClient = new GitHubClient();
-    @Getter private boolean oAuth = false;
+    @Getter
+    private boolean gitHubOAuth = false;
+    @Getter
+    private String gitHubUsername;
+    @Getter
+    private String gitHubPassword;
+    @Getter
+    private String oAuthToken;
 
     public void onEnable() {
         saveDefaultConfig();
-        oAuth = getConfig().getBoolean("OAuth.Enabled");
+        gitHubOAuth = getConfig().getBoolean("OAuth.Enabled");
+        if (isGitHubOAuth()) {
+            oAuthToken = getConfig().getString("OAuth.Token");
+        } else {
+            gitHubUsername = getConfig().getString("GitHub.Credentials.Username");
+            gitHubPassword = getConfig().getString("GitHub.Credentials.Password");
+        }
         getServer().getPluginManager().registerEvents(new PlayerJoinListener(this), this);
     }
 
